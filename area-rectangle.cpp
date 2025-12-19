357dd8ed259988c8be6b2591c2350c42e985c81d
0xnrous <0xnrous@gmail.com>
2025-12-19 11:16:18 +0100

    first program

---
+++ area-rectangle.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Issa_db on 19/12/2025.
+// Create program to Calculate the Area of a Rectangle
+//
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+    // create var
+    int length = 6;
+    int width = 4;
+
+    // equation Calculate the area of a rectangle
+    double area  = length * width;
+    // print statements
+    std::cout << "Length: "<< length << std::endl;
+    std::cout<<"Width: "<< width << std::endl;
+    std::cout <<"The Area: "<< area <<" cm"<<std::endl;
+    return 0;
+
+}
+
